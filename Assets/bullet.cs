f4790a243d9bd2e48271962aca30a80b8d7bb0a0
minhquanbit230339 <quan48926@gmail.com>
2026-01-25 21:14:40 +0700

    full

---
+++ Assets/bullet.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float flySpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = transform.position;
+        newPosition.y += Time.deltaTime * flySpeed;
+        transform.position = newPosition;
+    }
+}
