ca9d550a2756846d7dcfe076dc5524de2df9a473
minhquanbit230339 <quan48926@gmail.com>
2026-02-09 10:57:08 +0700

    ok

---
+++ Assets/Space Shooter Template FREE/Prefabs/EnemyAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public EnemyHealth health;
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+
+        if (playerHealth != null)
+        {
+            // gây damage cho player
+            playerHealth.TakeDamage(damage);
+
+            // enemy tự chết
+            health.TakeDamage(999);
+        }
+    }
+}
